cfe9053e54131e8a2ced5ff0bcf0a639c95f435d
ewadyrda <73639161+ewadyrda@users.noreply.github.com>
2020-11-19 09:04:15 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,30 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+	int liczba_statkow=0;
+	Stocznia stocznia{};
+	
+	while(toawr>0)
+		{
+			Statek* s1 = stocznia();
+			
+			if(typeid(*(statek))==typeid(Zaglowiec))
+			{ 
+				liczba_statkow++;
+			}
+        
+			if(towar>statek->transportuj()
+			{
+				towar-=statek->transportuj();
+				delete statek;
+
+			}
+               
+			else
+			{
+				delete statek;
+				break;
+			}
+		}
+    	return liczba_statkow;
 }
